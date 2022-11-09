9e698c74e4effafee94b8e2f5f9b6835857d9b97
Maddy798 <hardiktaneja798@gmail.com>
2022-11-09 10:23:56 +0530

    added firebase config file

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCgypJmJkycxeryGWBawzakxfJXcwd3tjI",
+  authDomain: "cheemstagram.firebaseapp.com",
+  projectId: "cheemstagram",
+  storageBucket: "cheemstagram.appspot.com",
+  messagingSenderId: "427895683510",
+  appId: "1:427895683510:web:ad1d4418a7e960ead66917",
+};
+
+const app = initializeApp(firebaseConfig);
