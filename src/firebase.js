80bfd1dfa5ace5cc665ee74ec7320e44676b3cf6
schwitz <koolharshbhardwaj526@gmail.com>
2022-11-09 16:04:38 +0530

    Added auth

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { GoogleAuthProvider,signInWithPopup } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCgypJmJkycxeryGWBawzakxfJXcwd3tjI",
@@ -9,5 +10,17 @@ const firebaseConfig = {
   messagingSenderId: "427895683510",
   appId: "1:427895683510:web:ad1d4418a7e960ead66917",
 };
-
 const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+export const SignInWithGoogle = ()  => {
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
